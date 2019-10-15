af312107063ac025df388696704a1b1a7e063b4c
orxanismayilov <orxan.ismayilov@outlook.com>
2019-10-15 22:50:12 +0400

    Refactoring

---
+++ src/main/java/com/orxan/sweetstorerest/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.orxan.sweetstorerest.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("orxan")
+                .password("orxan")
+                .roles("ADMIN");
+    }
+}
